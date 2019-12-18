7967cfe773033967877df3d69ae87e24a7fe7ff6
Vlad Klaus <morozvv@ukr.net>
2019-12-18 17:19:13 +0200

    ivent model create

---
+++ event_registrator/Models/Ivent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace event_registrator.Models
+{
+    public class Ivent
+    {
+        [Key]
+        public int IventId { get; set; }
+        [Required]
+        [Column(TypeName ="nvarchar(100)")]
+        public string Title { get; set; }
+        [Column(TypeName = "nvarchar(512)")]
+        public string Description { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string ImagePath { get; set; }
+    }
+}
