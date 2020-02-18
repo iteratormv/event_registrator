573746af5ed0f7542f2e99b1e9001105171be3e3
Vlad Klaus <morozvv@ukr.net>
2020-02-18 14:25:07 +0200

    added information about event in model

--- event_registrator/Models/Event.cs
+++ event_registrator/Models/Event.cs
@@ -12,12 +12,22 @@ namespace event_registrator.Models
         [Key]
         public int Id { get; set; }
         [Required]
-        [Column(TypeName ="nvarchar(250)")]
+        [Column(TypeName ="nvarchar(500)")]
         public string Title { get; set; }
-        [Column(TypeName = "nvarchar(1000)")]
+        [Column(TypeName = "nvarchar(3000)")]
         public string Description { get; set; }
-        [Column(TypeName = "nvarchar(250)")]
+        [Column(TypeName = "nvarchar(300)")]
         public string imagePath { get; set; }
+        [Column(TypeName = "nvarchar(300)")]
+        public string locationAdress { get; set; }
+        [Column(TypeName = "nvarchar(300)")]
+        public string locationDescription { get; set; }
+        [Column(TypeName = "nvarchar(300)")]
+        public string addedInformation1 { get; set; }
+        [Column(TypeName = "nvarchar(300)")]
+        public string addedInformation2 { get; set; }
+        [Column(TypeName = "nvarchar(300)")]
+        public string addedInformation3 { get; set; }
         [Required]
         public DateTime dateStart { get; set; }
         [Required]
